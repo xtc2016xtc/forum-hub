54384244a53d57299bc5b0c3035cd122e2e25f4b
MioTakaki[T3-1] <728611827@qq.com>
2026-02-10 11:16:13 +0800

    初始化

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+      <div>
+        <Button className="text-red-500">
+          点击
+        </Button>
+      </div>
+  )
+}
+
+export default Home
